1fc51ec9291a066a07d52780a2b22db694c80c23
teja679 <lovlytejaa@gmail.com>
2022-11-27 12:49:21 +0530

    search results

--- src/Components/SearchResults/index.js
+++ src/Components/SearchResults/index.js
@@ -1,9 +1,78 @@
-import React from 'react'
+import { MoreVert } from "@mui/icons-material";
+import { IconButton, Typography } from "@mui/material";
+import React from "react";
+import {
+  SearchResultsTitle,
+  SearchResultWrapper,
+  StyledResultsContainer,
+} from "./styled";
 
-function SearchResults({ data }) {
+function SearchResults({ data = {} }) {
   return (
-    <div>SearchResults</div>
-  )
+    <div>
+      <StyledResultsContainer>
+        <Typography variant="p" component="p">
+          About {data?.searchInformation?.formattedTotalResults} results{" "}
+          {data?.searchInformation?.searchTime} seconds
+        </Typography>
+        {data?.items.map((item) => (
+          <SearchResultWrapper key={item?.cacheId}>
+            <div>
+              <SearchResultsTitle href={item?.link}>
+                {item?.pagemap?.cse_image?.length > 0 &&
+                  item?.pagemap?.cse_image[0]?.src && (
+                    <img
+                      src={item?.pagemap?.cse_image[0]?.src}
+                      alt="thumbnail"
+                    />
+                  )}
+                <Typography
+                  variant="p"
+                  component="p"
+                  sx={{
+                    color: "#bdc1c6",
+                    fontSize: "0.875rem",
+                    fontWeight: "400",
+                  }}
+                >
+                  {item?.displayLink}
+                </Typography>
+              </SearchResultsTitle>
+              <SearchResultsTitle href={item?.link}>
+              <Typography
+                  variant="p"
+                  component="p"
+                  sx={{
+                    color: "#bdc1c6",
+                    fontSize: "0.875rem",
+                    fontWeight: "400",
+                  }}
+                >
+                  {item?.title}
+                </Typography>
+              </SearchResultsTitle>
+              <Typography
+                  variant="p"
+                  component="p"
+                  sx={{
+                    color: "#bdc1c6",
+                    fontSize: "0.875rem",
+                    fontWeight: "400",
+                  }}
+                >
+                  {item?.snippet}
+                </Typography>
+            </div>
+            <div>
+              <IconButton>
+                <MoreVert sx={{ fontSize: "1.5rem", color: "#fff" }} />
+              </IconButton>
+            </div>
+          </SearchResultWrapper>
+        ))}
+      </StyledResultsContainer>
+    </div>
+  );
 }
 
-export default SearchResults
+export default SearchResults;
---
+++ src/Components/SearchResults/styled.js
@@ -0,0 +1,40 @@
+import { styled } from "@mui/system";
+
+const StyledResultsContainer = styled("div")`
+  max-width: 650px;
+  margin-left: 107px;
+  margin-bottom: 100px;
+  top: 160px;
+  position: fixed;
+  bottom: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow-y: scroll;
+  overfolw-x: hidden;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+const SearchResultWrapper = styled("div")`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 48px 0;
+`;
+const SearchResultsTitle = styled("a")``;
+
+export { SearchResultsTitle, StyledResultsContainer, SearchResultWrapper };
+
+// styles
+// display: flex;
+// flex-direction: column;
+// height: 100vh;
+// display: flex;
+// justify-content: space-between;
+// align-items: center;
+// width: 100vw;
+// // height: 140px;
+// z-index: 1;
+// position: sticky;
+// border-bottom: 1px solid rgb(95, 99, 104);
+// background-color: ${({ theme }) => theme.body.darkColor};
